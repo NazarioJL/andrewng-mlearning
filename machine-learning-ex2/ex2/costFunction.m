966e063522734aa3fabe0c43c5eb1e677be8e65f
NazarioJL <jlnazario@hotmail.com>
2017-03-12 23:45:04 -0700

    Rest of week 3 course work. - costFunction - costFunctionReg - Fixed plotData, had plotting in ex2 - predict - sigmoid

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -6,27 +6,15 @@ function [J, grad] = costFunction(theta, X, y)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = size(theta);
 
-% You need to return the following variables correctly 
-J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
+J = (1 / m) * sum(-y .* log(sigmoid(X*theta)) - (1 - y) .* (log(1-sigmoid(X*theta))));
 
+for j = 1:n
+    grad(j) = (1 / m) * sum((sigmoid(X*theta) - y) .* X(:,j));
+end
 
-
-
-
-
-
-
-% =============================================================
 
 end
